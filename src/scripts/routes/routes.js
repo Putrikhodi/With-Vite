4ea398e5ba1937d04656c6db085291033f6f550f
Putrikhodi <168696443+Putrikhodi@users.noreply.github.com>
2025-05-30 10:39:44 +0700

    update routes.js

--- src/scripts/routes/routes.js
+++ src/scripts/routes/routes.js
@@ -1,6 +1,6 @@
 import Home from '../pages/home/Home';
 import AboutPage from '../pages/about/about-page';
-import Login from '../pages/login/Login';
+import Login from '../pages/auth/login/Login';
 
 const routes = {
   '/': new Home(),
